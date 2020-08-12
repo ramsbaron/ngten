250884e3dd8fd8866bbc9cbcf55728384ddefc0d
ramsbaron <sharad@softrams.com>
2020-08-12 17:36:24 -0400

    1 file modified

--- src/app/common/app-header.ts
+++ src/app/common/app-header.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, AfterViewInit, OnDestroy } from '@angular/core';
+import { fromEvent, Subject } from 'rxjs';
+import { distinctUntilChanged, map, pairwise, takeUntil, throttleTime } from 'rxjs/operators';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+export enum VisibilityState {
+  Visible = 'visible',
+  Hidden = 'hidden'
+}
+
+export enum Direction {
+  None = 'None',
+  Up = 'Up',
+  Down = 'Down'
+}
 
 /**
  * @title Basic toolbar
@@ -7,8 +21,60 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   templateUrl: 'app-header.html',
   styleUrls: ['app-header.css'],
+  animations: [
+    trigger('scrollAnimation', [
+      state(VisibilityState.Visible, style({
+        transform: 'translateY(0)'
+      })),
+      state(VisibilityState.Hidden, style({
+        transform: 'translateY(-64px)' // adjust this to the height of your header
+      })),
+      transition(`${VisibilityState.Visible} => ${VisibilityState.Hidden}`, animate('250ms')),
+      transition(`${VisibilityState.Hidden} => ${VisibilityState.Visible}`, animate('250ms'))
+    ])
+  ]
 })
-export class AppHeader {}
+export class AppHeader implements AfterViewInit, OnDestroy {
+  private destroy$: Subject<boolean> = new Subject<boolean>();
+  isHeader1Visible = VisibilityState.Visible;
+  isHeader2Visible = VisibilityState.Hidden;
+  slideHeader2InAtPosition = 30;
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  ngAfterViewInit() {
+    // create an observable stream of scroll positions and map them to UP / DOWN directions
+    const content = document.querySelector('.scrollWrapper');
+    const scroll$ = fromEvent(window, 'scroll').pipe( // if the scroll events happen on your window you could use 'window' instead of 'content' here
+      throttleTime(10),
+      map(() => window.pageYOffset), // if you used 'window' above replace 'content.scrollTop' with 'window.pageYOffset'
+      pairwise(),
+      map(([y1, y2]): Direction => {
+        // console.log(y1, y2);
+        return (y2 < y1 ? Direction.Up : (y2 > this.slideHeader2InAtPosition ? Direction.Down : Direction.None));
+      }),
+      distinctUntilChanged(),
+      takeUntil(this.destroy$)
+    );
+
+    // subscribe to the UP / DOWN scroll direction stream and set the header state accordingly
+    scroll$.subscribe(dir => {
+      if (dir === Direction.Down) {
+        console.log('scrolling down', window.pageYOffset);
+        this.isHeader1Visible = VisibilityState.Hidden;
+        this.isHeader2Visible = VisibilityState.Visible;
+      } else {
+        console.log('scrolling up', window.pageYOffset);
+        this.isHeader1Visible = VisibilityState.Visible;
+        this.isHeader2Visible = VisibilityState.Hidden;
+      }
+    });
+  }
+
+}
 
 
 /**  Copyright 2020 Google LLC. All Rights Reserved.
